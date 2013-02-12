1da30f7cb9212e837a3aa3c8ba66f68a84d9987d
PMarine <marine.preveaux@gmail.com>
2013-02-12 19:38:04 +0100

    [*] Simplification of game menu

--- js/cls/Evenements.js
+++ js/cls/Evenements.js
@@ -1,3 +1,5 @@
+var oPage = document.getElementsByClassName('page');
+
 // Détecte le click pendant une partie, sur le bouton pause
 function pausePartie() {
 	document.getElementById('pause').style.display = 'block';
@@ -14,10 +16,10 @@ function reprendrePartie() {
 
 // Détecte le click pendant que le menu Pause est afficher pour retourner au menu
 function quitterPartie() {
-	document.getElementById('pause').style.display = 'none';
-	document.getElementById('terrain').style.display = 'none';
-	document.getElementById('top').style.display = 'none';
-	document.getElementById('new-game').style.display = 'none';
+	for(var i in oPage) {
+		if(oPage[i] instanceof Element)
+			oPage[i].style.display = 'none';
+	}
 	oPartie.oChrono.pause();
 	oPartie.bPause = true;
 	document.getElementById('hp').style.display = 'block';
@@ -25,10 +27,10 @@ function quitterPartie() {
 
 // Détecte le click pour lancer une nouvelle partie
 function nouvellePartie() {
-	document.getElementById('pause').style.display = 'none';
-	document.getElementById('terrain').style.display = 'none';
-	document.getElementById('top').style.display = 'none';
-	document.getElementById('hp').style.display = 'none';
+	for(var i in oPage) {
+		if(oPage[i] instanceof Element)
+			oPage[i].style.display = 'none';
+	}
 	oPartie.oChrono.pause();
 	oPartie.bPause = true;
 	document.getElementById('new-game').style.display = 'block';
